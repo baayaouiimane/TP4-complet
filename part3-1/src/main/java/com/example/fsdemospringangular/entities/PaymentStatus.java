c54be475a437e189c372862ae4c142b286941080
baayaouiimane <167249908+baayaouiimane@users.noreply.github.com>
2024-06-15 02:35:52 +0100

    Delete part3-1 directory

--- part3-1/src/main/java/com/example/fsdemospringangular/entities/PaymentStatus.java
+++
@@ -1,5 +0,0 @@
-package com.example.fsdemospringangular.entities;
-
-public enum PaymentStatus {
-    CREATED ,VALIDATED,REJECTED
-}
--- part3-1/src/main/java/com/example/fsdemospringangular/entities/PaymentType.java
+++
@@ -1,5 +0,0 @@
-package com.example.fsdemospringangular.entities;
-
-public enum PaymentType {
-    CASH,CHECK ,TRANSFER ,DEPOSIT
-}
